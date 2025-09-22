6cd2d8101809cfc4f8eb6b5d5cade2ea22ac612a
iwasMateo <iwasMateo@gmail.com>
2025-09-22 15:12:07 +0200

    Update OhGodWhatAmIDoing.c

--- OhGodWhatAmIDoing.c
+++ OhGodWhatAmIDoing.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 
-float sqrtoo()
-{
-	int nmbr = 42;
-	float temp1 = 0;
-	float temp2 = 0;
-	int tries = 1;
-	float result = -5;
-	printf("Start");
-	for(int i=0; i<5; i++)
-		if(temp1*temp1<nmbr) {
-			temp2 = temp1;
-			temp1 +=1.00/tries;
-			result = temp2;
-			printf("temp1<nmbr\n");
-		}
-		else if(temp1*temp1==nmbr) {
-			result = temp1*temp1;
-			printf("temp1==nmbr\n");
-			return result;
-		}
-		else if(temp1*temp1>nmbr) {
-			printf("temp1>nmbr\n");
-			temp2 = temp1-1/tries;
-			tries = tries*10;
-			result = temp2;
-		}
-	return result;
+float sqrtoo() {
+    int nmbr = 42;
+    float temp1 = 0.0f;
+    float temp2 = 0.0f;
+    int tries = 1;
+    float result = -1.0f;
+
+    printf("Start\n");
+
+    for (int i = 0; i < 100000; i++) {
+        if (temp1 * temp1 > nmbr) {
+            temp2 = temp1;
+            temp1 -= 1.0f / tries;
+            result = temp2;
+            tries *= 10; // refine step size
+        } 
+        else if (temp1 * temp1 == nmbr) {
+            result = temp1;
+            printf("Found exact square root\n");
+            return result;
+        } 
+        else {
+            temp1 += 1.0f / tries;
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    printf("sqrt(42) ≈ %f\n", sqrtoo());
+    return 0;
 }
