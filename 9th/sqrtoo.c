b3e89b4a41bb81355e3bfdc242c4b02eb5cf937c
iwasmateo <iwasmateo@gmail.com>
2025-09-22 17:39:07 +0200

    Made an input system so it's not stuck with 42 and made a new structure for future and past projects

---
+++ 9th/sqrtoo.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+float sqrtoo(int nmbr) {
+    float temp1 = 0.0f;
+    float temp2 = 0.0f;
+    int tries = 1;
+    float result = -1.0f;
+
+    printf("Start\n");
+
+    for (int i = 0; i < 100000; i++) {
+        if (temp1 * temp1 > nmbr) {
+            temp2 = temp1;
+            temp1 -= 1.0f / tries;
+            result = temp2;
+            tries *= 10; // refine step size
+        } 
+        else if (temp1 * temp1 == nmbr) {
+            result = temp1;
+            printf("Found exact square root\n");
+            return result;
+        } 
+        else {
+            temp1 += 1.0f / tries;
+        }
+    }
+
+    return result;
+}
+
+int main() {
+  printf("Input squared number here: ");
+  int y;
+  scanf("%d", &y);
+  printf("sqrt(42) ≈ %f\n", sqrtoo(y));
+  return 0;
+}
+
+int side() {
+  printf("Hehehehe hellööööö");
+  return 0;
+}
